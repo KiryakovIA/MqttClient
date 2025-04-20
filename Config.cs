7c23d708849af055c4b6f462bebb4c4f97a79879
Igor Kiryakov <igor22041985@rambler.ru>
2025-04-20 13:57:36 +0300

    Тестовый пример работы с MQTT брокером

---
+++ Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttClient
+{
+	internal class Config
+	{
+		public string Broker { get; set; }
+		public int Port { get; set; }
+		public string Topic { get; set; }
+		public string TopicName { get; set; }
+		public string TopicId { get; set; }
+
+		string username = "igor";
+		string password = "kiapass12";
+
+	}
+}

75b148743d19e229f7f7d84e50a8e75491e9bca2
Igor Kiryakov <igor22041985@rambler.ru>
2025-04-20 17:39:12 +0300

    Добавил конфиг файл для сохранения настроек. Передача данных в HA. Пока тестовая версия

--- Config.cs
+++ Config.cs
@@ -2,20 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace MqttClient
 {
 	internal class Config
 	{
-		public string Broker { get; set; }
-		public int Port { get; set; }
-		public string Topic { get; set; }
-		public string TopicName { get; set; }
-		public string TopicId { get; set; }
+		public static Config Data { get; private set; }
 
-		string username = "igor";
-		string password = "kiapass12";
+		public MqttConfig Mqtt { get; set; }
+
+		public static async Task<bool> LoadDataAsync()
+		{
+			try
+			{
+				string json = await File.ReadAllTextAsync("Config.json");
+				Data = JsonConvert.DeserializeObject<Config>(json);
+			}
+			catch
+			{
+			}
+			return Data != null;
+		}
+
+		public class MqttConfig
+		{
+			public string Server { get; set; }
+			public int Port { get; set; }
+			public string BaseTopic { get; set; }
+			public string User { get; set; }
+			public string Password { get; set; }
+		}
 
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,71 +1,77 @@
 ﻿using System.Text;
+using MqttClient;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Protocol;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-class Program
+internal class Program
 {
-    static async Task Main(string[] args)
-    {
-		string broker = "192.168.1.133";
-		int port = 1883;
-		string clientId = Guid.NewGuid().ToString();
-		string topic = "Csharp/mqtt";
-		string username = "igor";
-		string password = "";
+	private static async Task Main(string[] args)
+	{
+		if (!await Config.LoadDataAsync())
+		{
+			Console.WriteLine("Не удалось загрузить конфигурацию");
+			return;
+		}
 
 		// Create a MQTT client factory
 		var factory = new MqttFactory();
 
-        // Create a MQTT client instance
-        var mqttClient = factory.CreateMqttClient();
+		// Create a MQTT client instance
+		var mqttClient = factory.CreateMqttClient();
 
-        // Create MQTT client options
-        var options = new MqttClientOptionsBuilder()
-            .WithTcpServer(broker, port) // MQTT broker address and port
-            .WithCredentials(username, password) // Set username and password
-            .WithClientId(clientId)
-            .WithCleanSession()
-            .Build();
+		// Create MQTT client options
+		var options = new MqttClientOptionsBuilder()
+			.WithTcpServer(Config.Data.Mqtt.Server, Config.Data.Mqtt.Port) // MQTT broker address and port
+			.WithCredentials(Config.Data.Mqtt.User, Config.Data.Mqtt.Password) // Set username and password
+			.WithClientId("MqttClient")
+			.WithCleanSession()
+			.Build();
 
-        // Connect to MQTT broker
-        var connectResult = await mqttClient.ConnectAsync(options);
+		// Connect to MQTT broker
+		var connectResult = await mqttClient.ConnectAsync(options);
 
-        if (connectResult.ResultCode == MqttClientConnectResultCode.Success)
-        {
-            Console.WriteLine("Connected to MQTT broker successfully.");
+		if (connectResult.ResultCode == MqttClientConnectResultCode.Success)
+		{
+			Console.WriteLine("Connected to MQTT broker successfully.");
 
-            // Subscribe to a topic
-            await mqttClient.SubscribeAsync(topic);
+			// Subscribe to a topic
+			await mqttClient.SubscribeAsync(Config.Data.Mqtt.BaseTopic + "/kwh");
 
-            // Callback function when a message is received
-            mqttClient.ApplicationMessageReceivedAsync += e =>
-            {
-                Console.WriteLine($"Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment)}");
-                return Task.CompletedTask;
-            };
+			// Callback function when a message is received
+			mqttClient.ApplicationMessageReceivedAsync += e =>
+			{
+				Console.WriteLine($"Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment)}");
+				return Task.CompletedTask;
+			};
 
-            // Publish a message 10 times
-            for (int i = 0; i < 10; i++)
-            {
-                var message = new MqttApplicationMessageBuilder()
-                    .WithTopic(topic)
-                    .WithPayload($"Hello, MQTT! Message number {i}")
-                    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
-                    .WithRetainFlag()
-                    .Build();
+			decimal value = 10;
+			// Publish a message 10 times
+			for (int i = 0; i < 100; i++)
+			{
+				string payload = JsonConvert.SerializeObject(new JObject(new JProperty("value", value)));
 
-                await mqttClient.PublishAsync(message);
-                await Task.Delay(1000); // Wait for 1 second
-            }
+				var message = new MqttApplicationMessageBuilder()
+					.WithTopic(Config.Data.Mqtt.BaseTopic + "/kwh")
+					.WithPayload(payload)
+					.WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+					.WithRetainFlag()
+					.Build();
 
-            // Unsubscribe and disconnect
-            await mqttClient.UnsubscribeAsync(topic);
-            await mqttClient.DisconnectAsync();
-        }
-        else
-        {
-            Console.WriteLine($"Failed to connect to MQTT broker: {connectResult.ResultCode}");
-        }
-    }
+				await mqttClient.PublishAsync(message);
+				await Task.Delay(5000); // Wait for 1 second
+				value++;
+			}
+
+			// Unsubscribe and disconnect
+			await mqttClient.UnsubscribeAsync(Config.Data.Mqtt.BaseTopic + "/kwh");
+			await mqttClient.DisconnectAsync();
+		}
+		else
+		{
+			Console.WriteLine($"Failed to connect to MQTT broker: {connectResult.ResultCode}");
+		}
+	}
 }
